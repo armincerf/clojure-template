4cb7e1b915f1bd49fc9331f13846ecdbe1d44e13
Alex Davis <alx@juxt.pro>
2020-08-01 19:35:36 +0100

    Add seeds for assets

--- PROJECTNAMESPACE.PROJECTNAME/src/PROJECTNAMESPACE/PROJECTNAME/api/seed.clj
+++ PROJECTNAMESPACE.PROJECTNAME/src/PROJECTNAMESPACE/PROJECTNAME/api/seed.clj
@@ -31,25 +31,42 @@
                        clojure.java.io/file
                        file-seq
                        remove-hidden)
-        documents (flatten
-                   (for [file (drop 1 seed-files)]
-                     (for [doc (json/parse-string
-                                (slurp file)
-                                (fn [k]
-                                  (keyword (.toLowerCase k))))
-                           :let [doc-type (-> file
-                                              .getName
-                                              (str/split #"\.")
-                                              first)]]
-                       (merge
-                        (namespace-keywords doc doc-type)
-                        {:crux.db/id (ids/gen-id doc-type)}
-                        (case doc-type
-                          "customer" {:customer/email
-                                      (-> doc
-                                          :name
-                                          (str/replace #" |'" "-")
-                                          (str "@fakemail.com"))}
-                          nil)))))]
-    (db/insert! node documents)))
+        documents-from-seed-files
+        (flatten
+         (for [file (drop 1 seed-files)]
+           (for [doc (json/parse-string
+                      (slurp file)
+                      (fn [k]
+                        (keyword (.toLowerCase k))))
+                 :let [doc-type (-> file
+                                    .getName
+                                    (str/split #"\.")
+                                    first)]]
+             (merge
+              (namespace-keywords doc doc-type)
+              {:crux.db/id (ids/gen-id doc-type)}
+              (case doc-type
+                "customer" {:customer/email
+                            (-> doc
+                                :name
+                                (str/replace #" |'" "-")
+                                (str "@fakemail.com"))}
+                nil)))))
+        customers (filter
+                   #(ids/customer? (:crux.db/id %))
+                   documents-from-seed-files)
+        assets
+        (for [customer customers
+              :let [asset-type (rand-nth [:email :phone :password])]]
+          {:crux.db/id (ids/asset)
+           :asset/customer (:crux.db/id customer)
+           :asset/type asset-type
+           :asset/data (case asset-type
+                         :email (:customer/email customer)
+                         :phone (str (rand-int 99999999))
+                         :password "bad password")
+           :asset/name "User defined name for data"
+           :asset/description "User defined description for data, could potentially be
+           quite long although probably not."})]
+    (db/insert! node (concat documents-from-seed-files assets))))
 
